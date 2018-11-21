bed0f5527b85755ffd6b4fbb058997ff588ff236
Nick Cabatoff <nick.cabatoff@gmail.com>
2018-11-21 09:52:38 -0500

    Rework vault configuration so it survives reboots.

--- files/vault.sh
+++ files/vault.sh
@@ -14,18 +14,9 @@ mkdir -p /etc/vault
 mkdir -p /var/vault
 
 cp /vagrant/supervisor.vault.conf /etc/supervisor/conf.d/vault.conf
+cp /vagrant/vault-wrapper.sh /usr/local/bin/vault-wrapper.sh
 
 supervisorctl reread
 supervisorctl update vault
 
 sleep 5
-
-export VAULT_ADDR=http://127.0.0.1:8200
-TOKEN=$(grep 'Root Token' /var/log/vault/out | tail -n1 | awk '{print $3}')
-echo $TOKEN | vault login -
-vault token create -id="1e9e1f5a-3c23-a5d2-d308-ed2c3dd541c4"
-vault login 1e9e1f5a-3c23-a5d2-d308-ed2c3dd541c4
-vault policy write secret /vagrant/acl.hcl
-vault write /auth/token/roles/nomad-cluster @/vagrant/nomad-cluster-role.json
-vault policy write nomad-server /vagrant/nomad-server-policy.hcl
-echo -n "12345" | vault kv put secret/password value=-
